1968046bb70dc2a62f576bbaf19b4ab782db4d56
FjiiNamiki <archerynamiki@gmail.com>
2021-07-28 15:07:10 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/
-  validates :encrypted_password, presence: true, confirmation: true, format: { with: VALID_PASSWORD_REGEX }
-  validates :encrypted_password_confirmation, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :lastname_kanji, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :firstname_kanji, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :lastname_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
