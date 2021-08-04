703a0464e31cd509000b3130027babadf15519a6
FjiiNamiki <archerynamiki@gmail.com>
2021-08-04 11:57:33 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,22 @@
 class Item < ApplicationRecord
+  validates :title, :discription, :image, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_pay_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :transport_day_id, numericality: { other_than: 1 }
+
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { in: 300..9999999 }
 
 
 
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_pay
+  belongs_to :prefecture
+  belongs_to :transport_day
 end
