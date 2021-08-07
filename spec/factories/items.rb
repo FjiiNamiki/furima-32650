2ae7fe5554c2679afea8fc420be5f922a22b8147
FjiiNamiki <archerynamiki@gmail.com>
2021-08-07 21:46:02 +0900

    商品出品機能修正1
    テストコード中のカラムの値を数値に修正。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     title            { 'オムライス' }
     discription      { '美味しいオムライス' }
-    category_id      { '2' }
-    condition_id     { '2' }
-    shipping_pay_id  { '2' }
-    prefecture_id    { '30' }
-    transport_day_id { '2' }
+    category_id      { 2 }
+    condition_id     { 2 }
+    shipping_pay_id  { 2 }
+    prefecture_id    { 30 }
+    transport_day_id { 2 }
     price            { 500 }
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,27 +31,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Discription can't be blank"
       end
       it 'categry_idが1では登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category must be other than 1"
       end
       it 'condition_idが1では登録できない' do
-        @item.condition_id = '1'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Condition must be other than 1"
       end
       it 'shipping_pay_idが1では登録できない' do
-        @item.shipping_pay_id = '1'
+        @item.shipping_pay_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping pay must be other than 1"
       end
       it 'prefecture_idが1では登録できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture must be other than 1"
       end
       it 'transport_day_idが1では登録できない' do
-        @item.transport_day_id = '1'
+        @item.transport_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Transport day must be other than 1"
       end
