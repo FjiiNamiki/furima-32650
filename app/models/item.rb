935f58f33552cf03ba1fa96ea1c2080a7dcf333f
FjiiNamiki <archerynamiki@gmail.com>
2021-08-07 21:11:29 +0900

    Rubocopを実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,7 @@
 class Item < ApplicationRecord
   validates :title, :discription, :image, :price, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_pay_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :transport_day_id, numericality: { other_than: 1 }
-
-  validates :price, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, :condition_id, :shipping_pay_id, :prefecture_id, :transport_day_id, numericality: { other_than: 1 }
+  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
