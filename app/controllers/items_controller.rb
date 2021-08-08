c794c7ad2c5810805a379eb016f46487c03660b7
FjiiNamiki <archerynamiki@gmail.com>
2021-08-08 19:41:13 +0900

    show.rbの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:title, :discription, :category_id, :condition_id, :shipping_pay_id, :prefecture_id, :transport_day_id, :price, :image).merge(user_id: current_user.id)
