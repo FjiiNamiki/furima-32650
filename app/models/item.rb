5964696d6556eac1fa25a502c00c83c38a2bd5cd
FjiiNamiki <archerynamiki@gmail.com>
2021-08-08 12:56:24 +0900

    商品一覧表示のためにviewファイルとコントローラーを編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :shipping_pay_id, :prefecture_id, :transport_day_id, numericality: { other_than: 1 }
   validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   belongs_to :user
+  has_one :order
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
