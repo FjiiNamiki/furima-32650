33222acbec7998b3870a21ad5f2beeada2288006
FjiiNamiki <archerynamiki@gmail.com>
2021-08-14 19:59:06 +0900

    フォームオブジェクトとコントローラーを作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def index
+    @order_destination = OrderDestination.new
+  end
+
+  def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:zipcode, :prefecture, :city, :adress, :building, :phonenumber).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
---
+++ app/models/order_destination.rb
@@ -0,0 +1,17 @@
+class OrderDestination
+  include ActiveModel::Model
+  attr_accessor :token, :user_id, :item_id, :zipcode, :prefecture, :city, :adress, :building, :phonenumber
+  with_options presence: true do
+    validates :token, :city, :adress, :user_id, :item_id
+    validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :phonenumber, format: {with: /\A\d{11}\z/}
+  end
+  validates :prefecture_id numericality:, { other_than: 1 }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(zipcode: zipcode, prefecture: prefecture, city: city, adress: adress, building: building, phonenumber: phonenumber, order_id: order.id)
+  end
+
+
+end
